61c94f2c2d1268d226affda2bc155f2e95e070fa
scarapa <scarapa@gmail.com>
2020-11-12 15:42:43 -0300

    Ref #1

--- lib/pages/home_tela.dart
+++ lib/pages/home_tela.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import '../widgets/adicionar_nova_pasta_modal.dart';
 import '../models/configuracao_model.dart';
 import '../models/diretorio_model.dart';
 import '../models/permissao_model.dart';
@@ -26,7 +27,8 @@ class _HomeTelaState extends State<HomeTela> {
   String _visualizacao;
   int _visualizacaoCrossAxisCount = 2;
 
-  List<Widget> bodyPage = new List<Widget>();
+  List<Widget> _bodyPage = new List<Widget>();
+  Widget _bodyBottom ;
 
   @override
   void initState() {
@@ -73,8 +75,10 @@ class _HomeTelaState extends State<HomeTela> {
   @override
   Widget build(BuildContext context) {
 
+    _bodyPage.clear();
+
     if(_caminhoDiretorio != null ){
-      bodyPage.add(
+      _bodyPage.add(
           Expanded(
             flex: 1,
             child: Text(_caminhoDiretorio ),
@@ -84,7 +88,7 @@ class _HomeTelaState extends State<HomeTela> {
 
       if(_visualizacao != null && _visualizacao == "grid"){
 
-        bodyPage.add(
+        _bodyPage.add(
           Expanded(
             flex: 19,
             child: Padding(
@@ -92,7 +96,7 @@ class _HomeTelaState extends State<HomeTela> {
               child: GridView.count(
                   crossAxisCount: _visualizacaoCrossAxisCount,
                   children: List.generate( _listaItens.length , (index) {
-                    return thumbGrid(context , _listaItens[index] );
+                    return thumbGrid(context: context , objeto: _listaItens[index], );
                   })
               ),
             ) ,
@@ -100,7 +104,7 @@ class _HomeTelaState extends State<HomeTela> {
         );
 
       }else if(_visualizacao != null && _visualizacao == "list" ) {
-        bodyPage.add(
+        _bodyPage.add(
             Expanded(
               flex: 19,
               child: Padding(
@@ -115,7 +119,7 @@ class _HomeTelaState extends State<HomeTela> {
             )
         );
     }else if( _visualizacao != null ){
-      bodyPage.add(
+      _bodyPage.add(
         Expanded(
           flex: 20,
           child: Center(
@@ -125,6 +129,108 @@ class _HomeTelaState extends State<HomeTela> {
       );
     }
 
+    var _existeItensSelecionado = true;
+    if(_existeItensSelecionado){
+      _bodyPage.add(
+        Expanded(
+          flex: 3 ,
+          child: Container(
+            padding: EdgeInsets.all(5),
+            decoration: new BoxDecoration(
+              color: Theme.of(context).accentColor,
+            ),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 80,
+                  padding: EdgeInsets.all(5),
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(16.0),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.copy , color: Colors.white , size: 26, ),
+                      Text("Copiar" ,style: TextStyle(color: Colors.white ) , ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  width: 80,
+                  padding: EdgeInsets.all(5),
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(16.0),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.cut , color: Colors.white , size: 26, ),
+                      Text("Recortar" ,style: TextStyle(color: Colors.white ) , ),
+                    ],
+                  ),
+                ),
+
+                Container(
+                  width: 80,
+                  padding: EdgeInsets.all(5),
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(16.0),
+                    color: Colors.green,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.delete , color: Colors.white , size: 26, ),
+                      Text("Deletar" ,style: TextStyle(color: Colors.white ) , ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      // _bodyBottom = Theme(
+      //   data: Theme.of(context).copyWith(
+      //       canvasColor: Theme.of(context).accentColor,
+      //       primaryColor: Colors.white,
+      //       textTheme: Theme.of(context).textTheme.copyWith(
+      //           caption: TextStyle(color: Colors.white54)
+      //       )
+      //   ),
+      //   child: BottomNavigationBar(
+      //     // currentIndex: _page,
+      //     onTap: (page){
+      //       // _pageController.animateToPage(
+      //       //     page,
+      //       //     duration: Duration(milliseconds: 500),
+      //       //     curve: Curves.ease
+      //       // );
+      //     },
+      //     items: [
+      //       BottomNavigationBarItem(
+      //           icon: Icon(Icons.copy),
+      //           title: const Text("Copiar")
+      //       ),
+      //       BottomNavigationBarItem(
+      //           icon: Icon(Icons.cut),
+      //           title: const Text("Recortar")
+      //       ),
+      //       BottomNavigationBarItem(
+      //           icon: Icon(Icons.delete),
+      //           title: const Text("Deletar")
+      //       )
+      //     ],
+      //   ),
+      // );
+    }
+      
+      
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
@@ -133,16 +239,17 @@ class _HomeTelaState extends State<HomeTela> {
         ],
       ),
       body: Column(
-            children: bodyPage.map((e) {
-                return e;
-              }).toList(),
-          )
+        children: _bodyPage.map((e) {
+          return e;
+        }).toList(),
+      ),
+      bottomNavigationBar: _bodyBottom ,
     );
   }
   _opcaoPopMenuButton(String opcao){
     if(opcao == "nova-pasta"){
       // chamar modal para criar nova pasta
-      _modalAdicionarPasta( context );
+      _modalAdicionarNovaPasta( context );
     }else if(opcao == "visualizar"){
       // chamar moal com opcoes de vizual
     }else if( opcao == "atualizar"){
@@ -154,59 +261,67 @@ class _HomeTelaState extends State<HomeTela> {
 
   }
 
-  _modalAdicionarPasta(BuildContext context) async {
+  _modalAdicionarNovaPasta(BuildContext context) async {
+
     var retornoLista = await showDialog(
         context: context,
         builder: (context) {
-          return Center(
-            child: Container(
-              padding: EdgeInsets.all(7),
-              width: 250,
-              height: 300,
-              decoration: BoxDecoration(
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Text("Criar Pasta" ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(5),
-                    child: TextFormField(
-                      //controller: _nomeNovaPastaController,
-                      decoration: InputDecoration(
-                          labelText: "Nova Pasta"
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      RaisedButton(
-                        onPressed: null ,
-                        child: Text("Cancelar"),
-                        color: Theme.of(context).primaryColor,
-                        textColor: Theme.of(context).textTheme.button.color,
-                      ),
-                      RaisedButton(
-                        onPressed: null ,
-                        child: Text("OK"),
-                        color: Theme.of(context).primaryColor,
-                        textColor: Theme.of(context).textTheme.button.color,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          );
+          return ModalAdicionarNovaPasta();
         }
     );
-    //SE A LISTA VIER PREENCHIDA
-    if(retornoLista != null) {
-      //adicionarConcentrado(int.parse( retornoLista[0] ) , int.parse( retornoLista[1] ) , double.parse( retornoLista[2] ) );
-    }
+
+    // var retornoLista = await showDialog(
+    //     context: context,
+    //     builder: (context) {
+    //       return Center(
+    //         child: Container(
+    //           padding: EdgeInsets.all(7),
+    //           width: 250,
+    //           height: 300,
+    //           decoration: BoxDecoration(
+    //             color: Colors.white,
+    //           ),
+    //           child: Column(
+    //             children: [
+    //               Padding(
+    //                 padding: EdgeInsets.all(5),
+    //                 child: Text("Criar Pasta" ),
+    //               ),
+    //               Padding(
+    //                 padding: EdgeInsets.all(5),
+    //                 child: TextFormField(
+    //                   //controller: _nomeNovaPastaController,
+    //                   decoration: InputDecoration(
+    //                       labelText: "Nova Pasta"
+    //                   ),
+    //                 ),
+    //               ),
+    //               Row(
+    //                 children: [
+    //                   RaisedButton(
+    //                     onPressed: null ,
+    //                     child: Text("Cancelar"),
+    //                     color: Theme.of(context).primaryColor,
+    //                     textColor: Theme.of(context).textTheme.button.color,
+    //                   ),
+    //                   RaisedButton(
+    //                     onPressed: null ,
+    //                     child: Text("OK"),
+    //                     color: Theme.of(context).primaryColor,
+    //                     textColor: Theme.of(context).textTheme.button.color,
+    //                   ),
+    //                 ],
+    //               )
+    //             ],
+    //           ),
+    //         ),
+    //       );
+    //     }
+    // );
+    // //SE A LISTA VIER PREENCHIDA
+    // if(retornoLista != null) {
+    //   //adicionarConcentrado(int.parse( retornoLista[0] ) , int.parse( retornoLista[1] ) , double.parse( retornoLista[2] ) );
+    // }
   }
   void _listenForPermissionStatus() async {
     final status = await PermissaoModel.getStatus();
